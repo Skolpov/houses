61970b4e817f777a8e28e2456cc76d625c88680c
Skolpov <finzio@ya.ru>
2021-03-23 21:41:17 +0600

    about us

--- #src/js/scroll-header.js
+++ #src/js/scroll-header.js
@@ -12,10 +12,12 @@ var content = document.querySelector('.banner__container');
 function myFunction() {
 	if (window.pageYOffset > sticky) {
 		header.classList.add("header--fixed");
+		header.classList.add('animate__animated', 'animate__fadeIn');
 		content.style.paddingTop="75px";
 	} else {
 		header.classList.remove("header--fixed");
 		content.style.paddingTop="";
+		header.classList.remove('animate__animated', 'animate__fadeIn');
 	}
 };
 
